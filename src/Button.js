f8a3f4935f46817a0ce3bc71229ef7cead0e979c
Govshru <shruti.gorivale2002@gmail.com>
2024-12-05 13:12:48 +0530

    upload

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Button() {
+  return (
+    
+    <div className="Button">
+        <h1>Hello World</h1>
+      <button>aboutus</button>
+      <button>contact</button>
+      
+      
+    </div>
+    
+  );
+}
+
+export default Button;
